0344a78029636ac54778d5323653a2d10fbb0c2b
Luis Alejandro Vera Moreno <alejandrovemo@gmail.com>
2023-07-21 19:41:51 -0500

    Initial React-Proyect-Based-Template

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+
+        </Routes>
+      </BrowserRouter>      
+    </div>
+  );
+}
+
+export default App;
