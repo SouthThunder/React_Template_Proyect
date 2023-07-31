42fe6ddcdcabfafe40138dd676b3ac0f566567be
Luis Alejandro Vera Moreno <alejandrovemo@gmail.com>
2023-07-30 20:47:21 -0500

    added TailWindCss to Proyect structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <div className="bg-black" >
       <BrowserRouter>
         <Routes>
 
