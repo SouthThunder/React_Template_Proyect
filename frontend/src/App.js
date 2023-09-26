a5efc1ba6113f437ba1a6eb91d1e581507aa4725
Luis Alejandro Vera Moreno <alejandrovemo@gmail.com>
2023-09-26 16:04:52 -0500

    update: build

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div className="">
+
+      <p>Hi Mom</p>
+      <BrowserRouter>
+        <Routes>
+
+        </Routes>
+      </BrowserRouter>   
+    </div>
+  );
+}
+
+export default App;
